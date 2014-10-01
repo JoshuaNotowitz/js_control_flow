ce1ebe921a1abb6c6b3915713ce54c901bf74220
JoshuaNotowitz <Joshua@Joshuas-MacBook-Pro.local>
2014-10-01 10:44:41 -0700

    Function lab 1

---
+++ josh_notowitz/Js_Function_lab_1.js
@@ -0,0 +1,8 @@
+function calculateAge(birthYear, currentYear) {
+    return currentYear - birthYear
+}
+
+var age = calculateAge(1997, 2014) 
+var age2 = calculateAge(1997, 2014) -1
+
+console.log("You are either "+ age + " or " + age2 + " years old")
